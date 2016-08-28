de5357492e48454095e5e89fb1ac9edeb348b605
ken-hiraken <kenichiro1030@gmail.com>
2016-08-28 17:43:27 +0900

    ログイン時、非ログイン時のビューの使い分け

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,24 @@
 class TweetsController < ApplicationController
 
-  def index
-    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
-  end
+    before_action :move_to_index, except: :index
 
-  def new
-  end
+    def index
+      @tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
+    end
 
-  def create
-    Tweet.create(tweet_params)
-  end
+    def new
+    end
 
-  private
-  def tweet_params
-    params.permit(:name, :image, :text)
-  end
+    def create
+      Tweet.create(tweet_params)
+    end
 
-end
+    private
+    def tweet_params
+      params.permit(:name, :image, :text)
+    end
+
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
+  end
