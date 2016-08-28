a5a81e7db366afcfc70e31566a638cda253ea7e6
ken-hiraken <kenichiro1030@gmail.com>
2016-08-28 18:11:38 +0900

    アソシエーションを定義した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets.page(params[:page]).per(5).order('created_at desc')
+
+    def show
+      @nickname = current_user.nickname
+      @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    end
+
   end
-end
