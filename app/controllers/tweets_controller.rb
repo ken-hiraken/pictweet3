e2d6fddb606ac7b192952b2a024810a68409d386
ken-hiraken <kenichiro1030@gmail.com>
2016-08-28 17:07:02 +0900

    一覧表示まで終了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,19 @@
 class TweetsController < ApplicationController
 
-    def index
-      @tweets = Tweet.all
-    end
-
-    def new
-    end
-
-    def create
-      Tweet.create(tweet_params)
-    end
-
-    private
-    def tweet_params
-      params.permit(:name, :image, :text)
-    end
-
+  def index
+    @tweets = Tweet.order("created_at DESC")
   end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
+end
